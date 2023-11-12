49db16e7228b84e6411af0bfb688522aa0cd070d
Pradeep-Deep14 <123350286+Pradeep-Deep14@users.noreply.github.com>
2023-11-12 15:40:26 +0530

    first commit

---
+++ Email Address.py
@@ -0,0 +1,18 @@
+#Email Address checking
+
+from re import *
+
+pattern=\
+compile("(^|\s)[-a-z0-9_.]+@([-a-z0-9]+\.)+[a-z]{2,6}(\s$|$)")
+
+def get_address():
+    address=input("Enter Email Address:")
+    is_valid=pattern.match(address)
+
+    if is_valid:
+        print("Valid address:",is_valid.group())
+
+    else:
+        print("Invalid address! Please try again....")
+
+get_address()
